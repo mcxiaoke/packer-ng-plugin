52ebd475ed15971f669228e54c88169073ff6174
mcxiaoke <mcxiaoke@gmail.com>
2014-12-17 18:14:41 +0800

    tweak scripts

--- test-build.sh
+++ test-build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+./deploy-local.sh
 echo "test clean build"
 cd sample
 ../gradlew clean build --refresh-dependencies --stacktrace $1 $2
--- test-market.sh
+++ test-market.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+./deploy-local.sh
 echo "test market and archives apk build."
 cd sample
 ../gradlew -Pmarket=markets.txt clean archiveApkRelease --refresh-dependencies --stacktrace $1 $2
