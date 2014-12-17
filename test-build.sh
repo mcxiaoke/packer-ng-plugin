78270517bb3c12218a93ccf697f7b67c1cbc89a9
mcxiaoke <mcxiaoke@gmail.com>
2014-12-17 15:46:35 +0800

    update test scripts

--- test-build.sh
+++ test-build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-./deploy-local.sh
 echo "test clean build"
 cd sample
-../gradlew clean build --stacktrace $1 $2
+../gradlew clean build --refresh-dependencies --stacktrace $1 $2
 cd ..
--- test-market.sh
+++ test-market.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-./deploy-local.sh
 echo "test market and archives apk build."
 cd sample
-../gradlew -Pmarket=markets.txt clean archiveApkRelease --stacktrace $1 $2
+../gradlew -Pmarket=markets.txt clean archiveApkRelease --refresh-dependencies --stacktrace $1 $2
 cd ..
