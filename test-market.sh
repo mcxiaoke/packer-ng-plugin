aa9abea41e92bbb505b33c9dcbd84ad2223ce285
mcxiaoke <mcxiaoke@gmail.com>
2014-12-23 17:53:38 +0800

    update test-market script

--- test-market.sh
+++ test-market.sh
@@ -2,5 +2,5 @@
 ./deploy-local.sh
 echo "test market and archives apk build."
 cd sample
-../gradlew -Pmarket=markets.txt clean archiveApkRelease --refresh-dependencies --stacktrace $1 $2
+../gradlew -Pmarket=markets.txt clean assembleRelease assembleBeta archiveApkRelease archiveApkBeta --refresh-dependencies --stacktrace $1 $2
 cd ..
