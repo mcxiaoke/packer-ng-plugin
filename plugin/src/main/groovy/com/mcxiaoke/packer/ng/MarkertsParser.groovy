b05252233aacbc52127a6a14bb89c3c3b9ae6bc8
唐帅 <tangshuai@talkweb.com.cn>
2017-06-02 15:15:57 +0800

    增加多Flavor的支持， 增加多Flavor时每个Flavor可设定自己的market列表的支持。

---
+++ plugin/src/main/groovy/com/mcxiaoke/packer/ng/MarkertsParser.groovy
@@ -0,0 +1,78 @@
+package com.mcxiaoke.packer.ng;
+
+import org.gradle.api.Project;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by tangshuai on 2017/6/2.
+ */
+
+public class MarkertsParser {
+
+    Project project;
+    String market;
+
+    MarkertsParser(Project project, String market) {
+        this.project = project;
+        this.market = market;
+    }
+
+    /**
+     * parse markets file
+     *
+     * @return found markets file
+     */
+    List<String> parseMarkets() {
+        List<String> markets = new ArrayList<String>();
+        debug("parseMarkets()" + market)
+
+//        if (!project.hasProperty(P_MARKET)) {
+//            debug("parseMarkets() market property not found, ignore")
+//            return false
+//        }
+
+        // check markets file exists
+        def marketsFilePath = this.market
+//        def marketsFilePath = project.property(P_MARKET).toString()
+        if (!marketsFilePath) {
+            println(":${project.name} markets property not found, using default")
+            // if not set, use default ./markets.txt
+            marketsFilePath = "markets.txt"
+        }
+
+        File file = project.rootProject.file(marketsFilePath)
+        if (!file.exists() || !file.isFile() || !file.canRead()) {
+//            throw new IllegalArgumentException("Invalid market file: ${file.absolutePath}")
+            return;
+        }
+        println(":${project.name} market: ${file.absolutePath}")
+        markets = readMarkets(file)
+        debug(":${project.name} found markets:$markets")
+        return markets
+    }
+
+
+    List<String> readMarkets(File file) {
+        // add all markets
+        List<String> allMarkets = []
+        file.eachLine { line, number ->
+            String[] parts = line.split('#')
+            if (parts && parts[0]) {
+                def market = parts[0].trim()
+                if (market) {
+                    allMarkets.add(market)
+                }
+            } else {
+                debug(":${project.name} skip invalid market line ${number}:'${line}'")
+            }
+        }
+        return allMarkets
+    }
+
+    void debug(String msg) {
+        project.logger.info(msg)
+    }
+}
--- plugin/src/main/groovy/com/mcxiaoke/packer/ng/PackerNgExtension.groovy
+++ plugin/src/main/groovy/com/mcxiaoke/packer/ng/PackerNgExtension.groovy
@@ -1,10 +1,12 @@
 package com.mcxiaoke.packer.ng
 
+import com.android.build.gradle.internal.dsl.ProductFlavor
 import org.gradle.api.Project
 
 // Android Packer Plugin Extension
 class PackerNgExtension {
-    static final String DEFAULT_NAME_TEMPLATE = '${appPkg}-${flavorName}-${buildType}-v${versionName}-${versionCode}'
+    static
+    final String DEFAULT_NAME_TEMPLATE = '${appPkg}-${flavorName}-${buildType}-v${versionName}-${versionCode}'
 
     /**
      *  archive task output dir
@@ -36,12 +38,40 @@ class PackerNgExtension {
      */
     String archiveNameFormat
 
+    String market
+
+    Map<String, String> flavorMarket = new HashMap<>()
+
+    List<String> markerts;
+
+    Project project
+
     PackerNgExtension(Project project) {
+        this.project = project
         archiveOutput = new File(project.rootProject.buildDir, "archives")
         tempOutput = new File(project.rootProject.buildDir, "temp")
         archiveNameFormat = DEFAULT_NAME_TEMPLATE
         checkSigningConfig = false
         checkZipAlign = false
+        market = new File(project.rootProject.buildDir, "channel.txt").absolutePath
+    }
+
+    public void market(String flavorName, String marketFile) {
+        project.logger.debug("market = ${marketFile},flavor = ${flavorName}")
+        flavorMarket.put(flavorName, marketFile)
+    }
+
+    @Override
+    protected PackerNgExtension clone() throws CloneNotSupportedException {
+        PackerNgExtension packerNgExtension = new PackerNgExtension(project)
+        packerNgExtension.flavorMarket = this.flavorMarket;
+        packerNgExtension.market = this.market;
+        packerNgExtension.archiveOutput = this.archiveOutput;
+        packerNgExtension.tempOutput = this.tempOutput;
+        packerNgExtension.checkSigningConfig = this.checkSigningConfig;
+        packerNgExtension.checkZipAlign = this.checkZipAlign;
+        packerNgExtension.archiveNameFormat = this.archiveNameFormat;
+        return packerNgExtension
     }
 
 
