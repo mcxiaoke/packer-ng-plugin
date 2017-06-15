4cde1c686f1946b8f85cdecc0ba75a14bb300c19
mcxiaoke <mcxiaoke@live.com>
2017-06-15 15:10:04 +0800

    rename sample to  app

--- test-market.sh
+++ test-market.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 ./deploy-local.sh
 echo "------ build for markets running..."
-./gradlew -Pmarket=markets.txt clean apkRelease $1 $2
+./gradlew -Pchannels=@channels/channels.txt clean apkRelease $1 $2
 echo "------ build for markets finished!"
